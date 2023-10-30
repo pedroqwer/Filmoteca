078251c8268646f0f74d66615b45509f98386d5a
pedroqwer <sanchezpere80@gmail.com>
2023-10-30 18:45:31 +0100

    Commit inicial.

---
+++ app/src/main/java/com/example/ejercicioentregapedro/FilmListActivity.java
@@ -0,0 +1,66 @@
+package com.example.ejercicioentregapedro;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FilmListActivity extends ArrayAdapter<Film> {
+    private  int rr;
+    private ArrayList<Film>films;
+    public FilmListActivity(@NonNull Context context, int resource, @NonNull ArrayList<Film> objects) {
+        super(context, resource, objects);
+        rr=resource;
+        films=objects;
+    }
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater=LayoutInflater.from(getContext());
+        View mi=inflater.inflate(rr,parent,false);
+
+        TextView titulo=mi.findViewById(R.id.titulo);
+        TextView director=mi.findViewById(R.id.director);
+        TextView genero =mi.findViewById(R.id.genero);
+        TextView anyo=mi.findViewById(R.id.anyo);
+        TextView formato=mi.findViewById(R.id.formato);
+        ImageView foto=mi.findViewById(R.id.img);
+        TextView enlaceF=mi.findViewById(R.id.enlace);
+        TextView comentario=mi.findViewById(R.id.comntario);
+
+        String tituliP=films.get(position).getTitle();
+        titulo.setText(tituliP);
+
+        String directorP=films.get(position).getDirector();
+        director.setText(directorP);
+
+        int generoP=films.get(position).getGenre();
+        genero.setText(generoP);
+
+        int anyoP =films.get(position).getYear();
+        anyo.setText(anyoP);
+
+        int formatoP=films.get(position).getFormat();
+        formato.setText(formatoP);
+
+        int fotoP=films.get(position).getImageResId();
+        foto.setImageResource(fotoP);
+
+        String enlaceP=films.get(position).getImdbUrl();
+        enlaceF.setText(enlaceP);
+
+        String comentarioP=films.get(position).getComments();
+        comentario.setText(comentarioP);
+
+        return  mi;
+    }
+}
---
+++ app/src/main/java/com/example/ejercicioentregapedro/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.ejercicioentregapedro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //FilmDataSource.Initialize();
+       // ListView lista=(ListView) findViewById(R.id.listasss);
+    //    FilmListActivity filmListActivity=new FilmListActivity(this,R.layout.mostrarlaspelis, FilmDataSource.films);
+
+      ///  lista.setAdapter(filmListActivity);
+        Button button=findViewById(R.id.boton1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url="https://github.com/pedroqwer/FilmotecaPedroSanchezBoix";
+
+                Intent intent=new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+
+                if(intent.resolveActivity(getPackageManager())!=null){
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+}
